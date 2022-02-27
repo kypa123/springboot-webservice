0fdb18eaae1fb7deddf612e111cc36f6802ba871
kypa123 <aksjajsxm123@gmail.com>
2022-02-28 00:59:41 +0900

    Adding and inheriting BaseTimeEntity

--- src/main/java/com/memefrg/springboot/service/posts/PostsService.java
+++ src/main/java/com/memefrg/springboot/service/posts/PostsService.java
@@ -1,12 +1,16 @@
 package com.memefrg.springboot.service.posts;
 import com.memefrg.springboot.domain.posts.Posts;
 import com.memefrg.springboot.domain.posts.PostsRepository;
+import com.memefrg.springboot.web.dto.PostsListResponseDto;
 import com.memefrg.springboot.web.dto.PostsResponseDto;
 import com.memefrg.springboot.web.dto.PostsSaveRequestDto;
 import com.memefrg.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +30,12 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
