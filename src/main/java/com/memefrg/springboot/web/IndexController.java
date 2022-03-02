080c6e1edfb1b1403609ea731d6f211be307f676
kypa123 <aksjajsxm123@gmail.com>
2022-03-02 17:43:50 +0900

    adding changes

--- src/main/java/com/memefrg/springboot/web/IndexController.java
+++ src/main/java/com/memefrg/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.memefrg.springboot.web;
 import com.memefrg.springboot.service.posts.PostsService;
+import com.memefrg.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,5 +15,17 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
