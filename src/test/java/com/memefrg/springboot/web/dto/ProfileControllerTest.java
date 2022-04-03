0a6734a21ce4fc62062fe4cca0e521043ce7749e
kypa123 <aksjajsxm123@gmail.com>
2022-04-04 02:10:53 +0900

    adding profileControllerTest

--- src/test/java/com/memefrg/springboot/web/dto/ProfileControllerTest.java
+++ src/test/java/com/memefrg/springboot/web/dto/ProfileControllerTest.java
@@ -21,7 +21,6 @@ public class ProfileControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-
     @Test
     public void profile은_인증없이_호출된다() throws Exception{
         String expected = "default";
@@ -29,5 +28,6 @@ public class ProfileControllerTest {
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(expected);
+        
     }
 }
