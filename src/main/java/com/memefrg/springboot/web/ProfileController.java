b11efdb4cb4ff69f75e0746bea2cb9e4f85f3436
kypa123 <aksjajsxm123@gmail.com>
2022-04-05 01:12:01 +0900

    changing errors

--- src/main/java/com/memefrg/springboot/web/ProfileController.java
+++ src/main/java/com/memefrg/springboot/web/ProfileController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
